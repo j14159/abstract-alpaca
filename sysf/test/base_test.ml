61630af0efb27e41463eb87ae367bc50b8d698eb
Jeremy Pierre <j.14159@gmail.com>
2020-05-26 06:22:04 -0700

    Fixed basic arrow typing test for sysf.
    Expanded and explained a bit, helping me work through System F in TAPL
    chapter 23.

--- sysf/test/base_test.ml
+++ sysf/test/base_test.ml
@@ -9,11 +9,24 @@ let test_abs_typ _ =
   assert_equal (Arrow (TUnit, TUnit)) res2 ~printer:string_of_typ
 
 let test_arrow_typing _ =
+  (* for_all[X].x:X.x  *)
   let abs = Type_abs ("X", Abs ("x", TVar "X", Var "x")) in
+  (* abs[() -> ()]  *)
   let of_arrow = Type_app (abs, Arrow (TUnit, TUnit)) in
   let t_of_arrow = typ_of (Env.create ()) of_arrow in
+  (* Expect t_of_arrow to be (() -> ()) -> () -> ()  *)
   assert_equal (Arrow (Arrow (TUnit, TUnit), Arrow (TUnit, TUnit))) t_of_arrow ~printer:string_of_typ;
-  assert_equal (TUnit) (typ_of (Env.create ()) (App (of_arrow, Unit))) ~printer:string_of_typ
+  (* unit_arrow is y:TUnit.y  *)
+  let unit_arrow = Abs ("y", TUnit, Var "y") in
+  (* Type of of_arrow(unit_arrow), or expanded:
+
+     (for_all[X].x:X.x[() -> ()]) (() -> ())
+
+   *)
+  let t_of_app = typ_of (Env.create ()) (App (of_arrow, unit_arrow)) in
+  (* Expect () -> ()  *)
+  let expected_t = Arrow (TUnit, TUnit) in
+  assert_equal expected_t t_of_app ~printer:string_of_typ
 
 let suite =
   "Base test suite (iterating in the beginning)" >:::
